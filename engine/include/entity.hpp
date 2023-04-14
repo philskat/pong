f89dcc7194f5c5688e3390f2611ab69ed7e76d9f
philskat <skatphil@gmail.com>
2023-04-14 14:02:49 +0200

    Added docs to header files

--- engine/include/entity.hpp
+++ engine/include/entity.hpp
@@ -4,6 +4,9 @@
 #include "model.hpp"
 #include "glm/glm.hpp"
 
+/**
+ * Represents an Entity in the game world 
+ */
 class Entity
 {
 private:
@@ -13,20 +16,81 @@ private:
   float m_Rotation;
 
 public:
+  /**
+   * Create Entity from Model
+   * 
+   * \parm model Model that should be used for the Entity
+   */
   Entity(Model* model);
+  /**
+   * Create Entity from Model and set Position
+   *
+   * \param model Model that should be used for the Entity
+   * \param position Position of the Entity
+   */
   Entity(Model* model, glm::vec2 position);
+  /**
+   * Create Entity from Model and set position and scale
+   * 
+   * \param model Model that should be used for the Entity
+   * \param position Position of the Entity
+   * \param scale Scale of the Entity
+   */
   Entity(Model* model, glm::vec2 position, glm::vec2 scale);
+  /**
+   * Create Entity from Modle and set position, scale and rotation
+   * 
+   * \param model Model that should be used for the Entity
+   * \param position Position of the Entity
+   * \param scale Scale of the Entity
+   * \param rotation Rotation of the Entity in degree
+   */
   Entity(Model* model, glm::vec2 position, glm::vec2 scale, float rotation);
 
+  /**
+   * Return the Model used by the Entity
+   *
+   * \return Model used by the Entity
+   */
   Model* getModel() const;
 
+  /**
+   * Set Position of the Entity
+   *
+   * \param position New position of the Entity
+   */
   void setPosition(glm::vec2 position);
+  /**
+   * Get Position of the Entity
+   *
+   * \return Current position of the Entity
+   */
   glm::vec2 getPosition() const;
 
+  /**
+   * Set Scale of the Entity
+   *
+   * \param New Scale of the Entity
+   */
   void setScale(glm::vec2 scale);
+  /**
+   * Get Scale of the Entity
+   *
+   * \return Current scale of the Entity
+   */
   glm::vec2 getScale() const;
 
+  /**
+   * Set Rotation of the Entity
+   * 
+   * \param rotation New Rotation of the Entity in degree
+   */
   void setRotation(float rotation);
+  /**
+   * Get Rotation of the Entity 
+   * 
+   * \return Current Rotation of the Entity in degree
+   */
   float getRotation() const;
 };
 
